8b541b261bd819544e5389608c32dc31a5538bbd
Sandeep Kolli <85800217+sandeep-kolli-007@users.noreply.github.com>
2023-03-07 01:25:43 +0530

    ,

--- index.js
+++ index.js
@@ -1,39 +1,41 @@
- 
-const cors = require('cors')({origin: true});
-const app = require('express')();
-app.use(cors);
+const express = require('express');
+const app = express();
+const server = require('http').createServer(app);
+const io = require('socket.io')(server);
+
+const cors = require('cors');
+
+// Set up CORS for Express
+app.use(cors());
 app.use(function(req, res, next) {
   res.header("Access-Control-Allow-Origin", "*");
   res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
   next();
 });
+// Set up CORS for Socket.io
+io.origins('*:*');
 
-const server = require('http').createServer(app);
-const io = require('socket.io')(server);
- 
-
-
+// Handle socket connection
 io.on('connection', (socket) => {
-    console.log('a user connected');
-  
-    socket.on('disconnect', () => {
-      console.log('user disconnected');
-    });
-  
-    socket.on('chat message', (msg) => {
-        console.log('message: ' + msg.message + ' from ' + msg.sender);
-        io.emit('chat message', msg);
-      });
+  console.log('a user connected');
+
+  socket.on('disconnect', () => {
+    console.log('user disconnected');
   });
-//
 
+  socket.on('chat message', (msg) => {
+    console.log('message: ' + msg.message + ' from ' + msg.sender);
+    io.emit('chat message', msg);
+  });
+});
 
+// Handle HTTP requests
 app.get('/', (req, res) => {
-  res.send('Hello from Firebase!' );
+  res.send('Hello World!');
 });
 
-var port = process.env.PORT || 3001;
-server.listen(port, function(){
-   console.log('listening in https://moments-node.web.app');
+// Start the server
+const PORT = process.env.PORT || 3000;
+server.listen(PORT, () => {
+  console.log(`Server listening on port ${PORT}`);
 });
- 
